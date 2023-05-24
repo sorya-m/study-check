65f5c3c1ced2255eaa3230b31ac82591aad4a2db
sorya-m <107857884+sorya-m@users.noreply.github.com>
2023-05-25 00:19:07 +0530

    Update next.js

--- next.js
+++ next.js
@@ -6,5 +6,21 @@ or
 npm install next@latest react@latest react-dom@latest
 
 
+Pages
+Routing:
+
+pages/index.js
+pages/blog.js
+
+Nesting Route:
+
+pages/blog/first.js
+pages/blog/second.js
+
+Dynamic Route:
+
+pages/product/[prodid].js
+
+
 
 
